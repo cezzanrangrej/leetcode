38236d8ec308a04131991445de4f584677641ab3
cezzan rangrej <cezzanrangrej23csds@rnsit.ac.in>
2026-08-04 23:24:12 +0530

    Time: 3 ms (79.93%) | Memory: 20.5 MB (36.49%) - LeetSync

--- 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.py
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.py
@@ -3,7 +3,7 @@ class Solution:
         n=len(numbers)
         left = 0
         right = n-1
-        for _ in range(n-1):
+        while left < right:
             som =  numbers[left] + numbers[right]
             if som ==target:
                 return [left+1, right+1]
